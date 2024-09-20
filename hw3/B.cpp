35b7ebe0f179f6be698830207bd9efa84edc18e2
dywsy21 <dywsy21@163.com>
2024-09-20 11:18:00 +0800

    chore: comment out unused Node and StaticList structures in B.cpp

--- hw3/B.cpp
+++ hw3/B.cpp
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
+#include <ext/rope>
 using namespace std;
 #define int long long
+using namespace __gnu_cxx;
 
-struct Node {
-    int val; Node* next = nullptr; Node* prev = nullptr;
-    Node(int v) {val = v;}
-};
+// struct Node {
+//     int val; Node* next = nullptr; Node* prev = nullptr;
+//     Node(int v) {val = v;}
+// };
 
-struct StaticList {
-    vector<Node*> node_ptrs;
-    StaticList(int n) {
-        for(int i {1}; i <= n; i++) {
-            Node* node_ptr = new Node(i);
-            node_ptrs.push_back(node_ptr);
-            if(i > 1) {
-                node_ptr->prev = node_ptrs[i - 1];
-                node_ptrs[i - 1]->next = node_ptr;
-            }
-        }
-    }
+// struct StaticList {
+//     vector<Node*> node_ptrs;
+//     StaticList(int n) {
+//         for(int i {1}; i <= n; i++) {
+//             Node* node_ptr = new Node(i);
+//             node_ptrs.push_back(node_ptr);
+//             if(i > 1) {
+//                 node_ptr->prev = node_ptrs[i - 1];
+//                 node_ptrs[i - 1]->next = node_ptr;
+//             }
+//         }
+//     }
 
-    int operator[](int index) {
-        return (node_ptrs[index])->val;
-    }
-};
+//     int operator[](int index) {
+//         return (node_ptrs[index])->val;
+//     }
+// };
 
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    deque<int> dq;
-
-
+    rope<int> r;
+    //TODO
 
     system("pause");
     return 0;
