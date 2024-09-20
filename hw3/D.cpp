c24c535d7565184a9d304cc7006b932199d88d95
dywsy21 <dywsy21@163.com>
2024-09-20 09:52:39 +0800

    refactor: simplify variable declarations and improve output formatting in D.cpp

--- hw3/D.cpp
+++ hw3/D.cpp
@@ -5,9 +5,7 @@ using namespace std;
 
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n;
-    string s;
-    cin >> n >> s;
+    int n; string s; cin >> n >> s;
 
     auto hasDuplicateSubstring = [&](int length) {
         if (length == 0) return true;
@@ -20,20 +18,19 @@ signed main(){
         return false;
     };
 
-    int left = 0, right = n, result = 0;
+    int left = 0, right = n, ans = 0;
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (hasDuplicateSubstring(mid)) {
-            result = mid;
+            ans = mid;
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
 
-    cout << result << endl;
+    cout << ans;
 
-
-    system("pause");
+    // system("pause");
     return 0;
 }
