fa9baf1d618ea2ce2d1fc9f4e8a9fb60615abb90
dywsy21 <dywsy21@163.com>
2024-09-20 10:22:01 +0800

    refactor: Simplify duplicate substring detection by using string substrings instead of hash functions

--- hw3/D.cpp
+++ hw3/D.cpp
@@ -8,30 +8,18 @@ signed main(){
     int n; string s; cin >> n >> s;
 
     auto hasDuplicateSubstring = [&](int length) {
-        const int p = 31;
-        const int m = 1e9 + 9;
-        vector<int> p_pow(n);
-        p_pow[0] = 1;
-        for (int i = 1; i < n; i++)
-            p_pow[i] = (p_pow[i - 1] * p) % m;
-
-        vector<long long> hashes(n + 1, 0);
-        for (int i = 0; i < n; i++)
-            hashes[i + 1] = (hashes[i] + (s[i] - 'a' + 1) * p_pow[i]) % m;
-
-        unordered_map<long long, int> seen;
+        unordered_map<string, int> seen; // val = starting index
         for (int i = 0; i <= n - length; i++) {
-            long long current_hash = (hashes[i + length] + m - hashes[i]) % m;
-            current_hash = (current_hash * p_pow[n - i - 1]) % m;
-            if (seen.count(current_hash) && i >= seen[current_hash] + length) {
+            string substring = s.substr(i, length);
+            if (seen.count(substring) && i >= seen[substring] + length) {
                 return true;
             }
-            seen[current_hash] = i;
+            seen[substring] = i;
         }
         return false;
-    };
+     };    
 
-    int left = 1, right = n, ans = 0;
+    int left = 1, right = n + 10, ans = 0;
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (hasDuplicateSubstring(mid)) {
