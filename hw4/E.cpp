8cbc133b6058f1b9d1a0f4fd9beb0c6e29adcc2b
dywsy21 <dywsy21@163.com>
2024-09-26 19:46:20 +0800

    fix: correct index handling and remove unused variable initialization in E.cpp

--- hw4/E.cpp
+++ hw4/E.cpp
@@ -4,14 +4,14 @@ using namespace std;
 using namespace __gnu_cxx;
 #define int long long
 
-const int N = 1e5 + 7;
+// #define DEBUG
 
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int T; cin >> T;
     while(T--) {
         string s; cin >> s;
-        string v1(N, '0'), v2(N, '0'); int index = 0;
+        string v1, v2; int index = 0;
 
 #ifdef DEBUG
         cout << "Processing string: " << s << endl;
@@ -22,7 +22,7 @@ signed main(){
             cout << "Current character: " << c << ", v1[index]: " << v1[index] << ", index: " << index << endl;
 #endif
             if(v1[index] == c){
-                index = (index + 1) % s.length(); v2 += (c);
+                index = (index + 1) % v1.length(); v2 += (c);
 #ifdef DEBUG
                 cout << "Characters matched. Incremented index: " << index << ", v2: " << v2 << endl;
 #endif
@@ -31,11 +31,12 @@ signed main(){
                 index = 0; v1 += v2;
                 v2.clear(); v1 += c;
 #ifdef DEBUG
-                cout << "Characters did not match. Reset index, updated v1: " << v1 << endl;
+                cout << "Characters did not match. Reset index, updated v1\n";
 #endif
             }
         }
-        cout << (int)s.length() - index << '\n';
+        int ans = v1.length() == 1 ? 0 : v1.length() - index;
+        cout << ans << '\n';
 #ifdef DEBUG
         cout << "Result for current string: " << (int)s.length() - index << endl;
 #endif
