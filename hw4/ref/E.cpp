79851f0a6ce56d1932f4152ec4c49d0687d4965d
dywsy21 <dywsy21@163.com>
2024-09-27 09:14:45 +0800

    feat: add E.cpp for homework 4 reference

---
+++ hw4/ref/E.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int maxn = 1e6 + 100;
+char a[maxn];
+int ne[maxn];
+int lena, lenb;
+void getnext()
+{
+    int j = 0, k = -1;
+    ne[0] = -1;
+    while (j < lena)
+    {
+        if (k == -1 || a[j] == a[k])
+        {
+            j++;
+            k++;
+            ne[j] = k;
+        }
+        else
+        {
+            k = ne[k];
+        }
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        scanf("%s", a);
+        lena = strlen(a);
+        getnext();
+        int ans = 0;
+        int m = lena - ne[lena]; // 循环节
+        if (lena % m == 0 && lena / m >= 2)
+        { // 至少右一个循环节
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << m - lena % m << endl;
+        }
+    }
+}
